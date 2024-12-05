223f9a48d3077c788973ae808afae9c3689e9bd3
EmilCh <EmilCh@users.noreply.github.com>
2024-12-05 19:26:07 +0100

    Update prep-debian.sh

--- prep-debian.sh
+++ prep-debian.sh
@@ -31,8 +31,8 @@ sed  -i -e "s#SystemdCgroup = false#SystemdCgroup = true#" /etc/containerd/confi
 systemctl restart containerd
 systemctl enable containerd
 
-echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /" | tee /etc/apt/sources.list.d/kubernetes.list
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.31/deb/ /" | tee /etc/apt/sources.list.d/kubernetes.list
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.31/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 
 
 apt update
